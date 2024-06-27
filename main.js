c77356bf8bff90fa1c34b83d98e889f24a7796d1
Lubna <muspaksamsu@gmail.com>
2024-06-27 22:02:21 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var person_name = "Lubna";
+// console.log(person_name.toLocaleLowerCase())
+//console.log(person_name.toUpperCase())
+console.log(person_name.charAt(0).toUpperCase() + person_name.slice(3));
---
+++ main.ts
@@ -0,0 +1,8 @@
+let person_name ="Lubna"
+// console.log(person_name.toLocaleLowerCase())
+//console.log(person_name.toUpperCase())
+console.log(person_name.charAt(0).toUpperCase()+person_name.slice(3))
+
+
+
+
